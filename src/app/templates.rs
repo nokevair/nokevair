b26f50b2f03820e0d910a242f922d0f7397f98c2
nokevair <64569057+nokevair@users.noreply.github.com>
2020-05-02 17:11:03 -0700

    factor about template into parent and child

--- src/app/templates.rs
+++ src/app/templates.rs
@@ -3,6 +3,7 @@ use tera::Tera;
 // TODO: make this return an Err instead of panicking so it can be caught
 pub fn load() -> Tera {
     let mut tera = Tera::default();
+    tera.add_template_file("templates/base.html.tera", Some("base.html")).unwrap();
     tera.add_template_file("templates/about.html.tera", Some("about.html")).unwrap();
     tera
 }
