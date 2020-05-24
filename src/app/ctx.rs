a6863760f2ed97d33c8551325f8bc9f30635e536
nokevair <64569057+nokevair@users.noreply.github.com>
2020-05-23 19:18:24 -0700

    Add note about `ctx` vs. `app_ctx` usage

--- src/app/ctx.rs
+++ src/app/ctx.rs
@@ -1,5 +1,9 @@
 //! Exposes a `Ctx` struct which provides various functions with shared access
 //! to data and functionality that is widely used throughout the server.
+//! 
+//! Most functions that require the context will take it as a parameter called `ctx`.
+//! In cases where this would be shadowed by the RLua context, it is instead called
+//! `app_ctx`.
 
 use std::sync::Arc;
 
